f092ba01d4a581644a0f75d5bfbd575b58355516
Osman <114867365+osmansam@users.noreply.github.com>
2023-03-09 21:11:52 -0500

    haber controller is set

---
+++ controllers/haberController.js
@@ -0,0 +1,68 @@
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
+const Haber = require("../models/Haber");
+
+//Get all Habers
+const getAllHabers = async (req, res, next) => {
+  const habers = await Haber.find();
+  res.status(StatusCodes.OK).json({
+    success: true,
+    habers,
+  });
+};
+//create Haber Image
+const createHaberImage = async (req, res) => {
+  const result = await cloudinary.uploader.upload(
+    req.files.image.tempFilePath,
+    {
+      use_filename: true,
+      folder: "news-upload",
+    }
+  );
+  fs.unlinkSync(req.files.image.tempFilePath);
+  return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
+};
+
+//create Haber
+const createHaber = async (req, res, next) => {
+  const haber = await Haber.create(req.body);
+  res.status(StatusCodes.CREATED).json({
+    success: true,
+    haber,
+  });
+};
+
+//update Haber
+const updateHaber = async (req, res) => {
+  const haber = await Haber.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!haber) {
+    throw new CustomError.NotFoundError("Haber not found");
+  }
+  res.status(StatusCodes.OK).json({
+    haber,
+  });
+};
+
+//delete Haber
+const deleteHaber = async (req, res) => {
+  const haber = await Haber.findOneAndDelete({ _id: req.params.id });
+  if (!haber) {
+    throw new CustomError.NotFoundError("Haber not found");
+  }
+  res.status(StatusCodes.OK).json({
+    haber,
+  });
+};
+
+module.exports = {
+  getAllHabers,
+  createHaberImage,
+  createHaber,
+  updateHaber,
+  deleteHaber,
+};
